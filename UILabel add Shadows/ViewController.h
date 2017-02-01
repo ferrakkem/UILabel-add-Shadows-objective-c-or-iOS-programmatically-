82cec835ae3fb18b1adbcd96ee00feb1f05c97b5
ferrakkem <ferrakkem@gmail.com>
2017-02-01 13:28:05 +0600

    Initial Commit

---
+++ UILabel add Shadows/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILabel add Shadows
+//
+//  Created by Ferrakkem Bhuiyan on 1/2/17.
+//  Copyright © 2017 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UILabel add Shadows/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UILabel add Shadows
+//
+//  Created by Ferrakkem Bhuiyan on 1/2/17.
+//  Copyright © 2017 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
